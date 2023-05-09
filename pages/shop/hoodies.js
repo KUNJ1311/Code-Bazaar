9af8b823cb32ebbd4e2f8ae010a240bc64e5d6dc
Kunj1311 <74526794+KUNJ1311@users.noreply.github.com>
2023-05-09 09:44:58 +0530

    changes responsive

--- pages/shop/hoodies.js
+++ pages/shop/hoodies.js
@@ -1,7 +1,5 @@
 import SubNavbar from "@/components/Shop/ProductCarousel";
 import React from "react";
-import { useState } from "react";
-import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 
 const Hoodies = () => {
 	const data = [
@@ -66,49 +64,11 @@ const Hoodies = () => {
 			name: "Mugs",
 		},
 	];
-	const [current, setCurrent] = useState(0);
-	const length = data.length;
-
-	const nextSlide = () => {
-		setCurrent(current === length - 1 ? 0 : current + 1);
-	};
-
-	const prevSlide = () => {
-		setCurrent(current === 0 ? length - 1 : current - 1);
-	};
 
 	return (
 		<>
 			<SubNavbar />
 			hoodie
-			{/* <div className="container mx-auto px-4">
-				<div className="flex text-4xl font-normal text-gray-900 justify-center text-center my-5 relative">
-					<h1 className="m-1">Product Category</h1>
-					<span className="absolute top-12 w-28 h-1 bg-primary border rounded-xl border-transparent"></span>
-				</div>
-				<div className="flex container my-5 mx-auto justify-start items-center space-x-4 relative">
-					{data.map((data, index) => (
-						<div key={index} className={`${index === current ? "opacity-100" : "opacity-0"} absolute transition-opacity`}>
-							<div className="px-2 pt-2 pb-3 border rounded-2xl border-[#cce7d0] flex justify-center flex-col space-y-3 shadow-lg hover:shadow-2xl transition-my">
-								<div className="flex border border-slate-300 rounded-2xl overflow-hidden w-32 h-32 ">
-									<a href={data.link}>
-										<img className="object-cover block " src={data.img} alt={data.name} width="128px" height="128px" />
-									</a>
-								</div>
-								<button className="flex text-lg justify-center items-center border   transition-my border-gray-300 rounded-md mx-4 font-medium hover:text-white hover:bg-primary shadow-md py-0">
-									<a href={data.link}>{data.name}</a>
-								</button>
-							</div>
-						</div>
-					))}
-					<button className="absolute top-1/2 transform -translate-y-1/2 left-0 bg-transparent hover:bg-gray-300 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-300 hover:border-transparent rounded" onClick={prevSlide}>
-						<FiChevronLeft />
-					</button>
-					<button className="absolute top-1/2 transform -translate-y-1/2 right-0 bg-transparent hover:bg-gray-300 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-300 hover:border-transparent rounded" onClick={nextSlide}>
-						<FiChevronRight />
-					</button>
-				</div>
-			</div> */}
 		</>
 	);
 };
--- pages/shop/mugs.js
+++ pages/shop/mugs.js
@@ -1,6 +1,4 @@
 import SubNavbar from "@/components/Shop/ProductCarousel";
-import Link from "next/link";
-import React, { useEffect, useRef, useState } from "react";
 
 const Mugs = () => {
 	return (
